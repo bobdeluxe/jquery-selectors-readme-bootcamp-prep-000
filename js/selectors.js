fe19ea0a4d19ea938a8cbe33e37da2eb811bd519
bob deluxe <learncobobdeluxe@gmail.com>
2018-03-11 15:44:27 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -5,3 +5,15 @@ function paragraphSelector() {
 function lastImageSelector() {
   return $('img:last')
 }
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('ul#pic-list:first-child')
+}
